d30b4fac99bec04c77cedabe5666467dab975497
Gaten <dongbin300@gmail.com>
2023-05-04 12:21:32 +0900

    Initial commit

---
+++ Upbit.Net.Tests/UpbitNetApiTests.cs
@@ -0,0 +1,16 @@
+namespace Upbit.Net.Tests
+{
+    public class UpbitNetApiTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
